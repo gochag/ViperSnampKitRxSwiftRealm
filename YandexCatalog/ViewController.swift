7f485305cec73b278e7c6b3b465c5bfd77120ff1
qochaq@gmail.com <arcenal24556>
2020-10-06 19:48:10 +0400

    Initial Commit

---
+++ YandexCatalog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YandexCatalog
+//
+//  Created by Tarlan Hekimzade on 06.10.2020.
+//  Copyright © 2020 Tarlan Hekimzade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
